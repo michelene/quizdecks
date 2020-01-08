f7946a252788c0ee7c532b0501798abcb5c18686
michelene <michelenechon@gmail.com>
2020-01-08 00:06:53 -0800

    Initial commit

---
+++ data/data.js
@@ -0,0 +1,9 @@
+// Our initial data structure will be an array of objects:
+let deck01 = [
+  { front: '2 + 2', back: '4' },
+  { front: '2 + 8', back: '10' },
+  { front: '10 * 4', back: '40' },
+  { front: '5 ^ 4', back: '625' },
+  { front: '9 / 3', back: '3' },
+  { front: '125 + 5', back: '130' }
+];
