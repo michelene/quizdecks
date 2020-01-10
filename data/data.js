1ac50b6976c1dce72e2fd0b59d4d4603d895d009
michelene <michelenechon@gmail.com>
2020-01-10 07:52:39 -0800

    Fix logic

--- data/data.js
+++ data/data.js
@@ -3,7 +3,8 @@ let deck01 = [
   { front: '2 + 2', back: '4' },
   { front: '2 + 8', back: '10' },
   { front: '10 * 4', back: '40' },
-  { front: '5 ^ 4', back: '625' },
-  { front: '9 / 3', back: '3' },
   { front: '125 + 5', back: '130' }
 ];
+
+// { front: '5 ^ 4', back: '625' },
+// { front: '9 / 3', back: '3' },
